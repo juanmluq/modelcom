7fd8196191f306ab747b29bc9cc5e0644ba330d1
juanmluq <soyluque@hotmail.com>
2022-05-28 10:49:38 -0300

    pase 1 mas

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,18 +1,45 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux'
+import { getAllProducts } from '../../redux/actions';
+import ProductCard from '../ProductCard/ProductCard';
 
 // Fijense en los test que SI O SI tiene que ser un class component, de otra forma NO VAN A PASAR LOS TEST.
 
 export class Home extends Component {
+  componentDidMount(){
+    this.props.getAllProducts()
+  }
+  
   render() {
     return (
-     <h1>Home</h1>
-      )
+      <div>
+     <h1>Henry Commerce</h1>
+     <img src="main-image-cp2.jpg" alt="main-img" />
+     <h3>Products</h3>
+     {this.props.products?.map (product => 
+     <ProductCard 
+     id= {product.id}
+     name= {product.name}
+     price= {product.price}
+     stock= {product.stock}
+     />
+     )
+}
+     </div>
+     )
   }
 }
 
-export const mapStateToProps = undefined;
+export const mapStateToProps = (state) => {
+  return {
+    products: state.products
+  }
+};
 
-export const mapDispatchToProps = undefined;
+export const mapDispatchToProps = (dispatch) => {
+  return {
+    getAllProducts: () => dispatch(getAllProducts())
+  }
+};
 
 export default connect(mapStateToProps,mapDispatchToProps)(Home)
