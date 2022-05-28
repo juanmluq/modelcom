1571dbb5659fb53895eecee833bb747746708cb6
juanmluq <soyluque@hotmail.com>
2022-05-28 11:03:08 -0300

    pase otro

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { deleteProduct } from '../../redux/actions';
+import { Link } from 'react-router-dom';
+// FIJENSE DE HACERLO SI O SI CON FUNCTIONAL COMPONENT! SI NO LOS TEST NO PASAN.
 
-  // FIJENSE DE HACERLO SI O SI CON FUNCTIONAL COMPONENT! SI NO LOS TEST NO PASAN.
 
+const ProductCard = ({name, image, id, stock, price}) => {
+  const dispatch = useDispatch()
 
-const ProductCard = () => {
-  
   return (
     <div>
-     Product Card
+     <button onClick={() => dispatch(deleteProduct(id))}>x</button>
+     <h3>{name}</h3>
+     <img src={image} alt={name} />
+     <p>Stock: {stock}</p>
+     <p>Precio: ${price}</p>
+     <Link to={`/product/${id}`}>{name}</Link>
     </div>
   );
 };
